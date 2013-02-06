ea5c4e4c571915c1aef50c9163a4aab34ce99eed
yongchunchengyc <ahhqcheng@me.com>
2013-02-06 17:23:15 +0800

    更优雅的实现FileChannel读取文件乱码的问题，充分利用了java.nio的新特性

--- nio/src/main/java/com/lp/liuan/FileChannelExample.java
+++ nio/src/main/java/com/lp/liuan/FileChannelExample.java
@@ -4,25 +4,29 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 
 public class FileChannelExample {
     public static void main(String args[]){
+    	Charset charset = Charset.forName("GBK");
+    	CharsetDecoder decoder = charset.newDecoder();
         FileInputStream fis = null;
         try {
             fis = new FileInputStream("E:/学习总结/java乱码本质.txt");
             FileChannel fileChannel = fis.getChannel();
             ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            CharBuffer charBuffer = CharBuffer.allocate(1024);
             int bytes = fileChannel.read(byteBuffer);
             while(bytes!=-1){
                 byteBuffer.flip();
-                byte[] buffer = new byte[bytes];//将读取的字节放到字节数组中，便于后面解决乱码问题
-                int i=0;
-                while (byteBuffer.hasRemaining()){
-                	buffer[i++]=byteBuffer.get();
-                }
-                String str = new String(buffer,"GBK");//解决乱码
-                System.out.println(str);
+                decoder.decode(byteBuffer, charBuffer, false);
+                charBuffer.flip();
+              
+                System.out.println(charBuffer);
+                charBuffer.clear();
                 byteBuffer.clear();
                 bytes = fileChannel.read(byteBuffer);
             }
